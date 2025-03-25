6dd228eea4d5afbecb629f4e7b5a9053a46556f0
YikesDML <david.leslie17@gmail.com>
2025-03-25 06:47:49 -0600

    D: Word counter CH2 L4

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def get_book_text(file):
         file_contents = f.read()
     return file_contents
 
+def count_words(file_contents):
+    words = file_contents.split()
+    return len(words)
+
 def main():
     contents = get_book_text("books/frankenstein.txt")
-    print(contents)
+    word_count = count_words(contents)
+    print(f"{word_count} words found in the document")
+
+#bookbot CH2 L3
+#def main():
+    #contents = get_book_text("books/frankenstein.txt")
+    #print(contents)
 
 main()
